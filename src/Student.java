e3b07f7182a68652cde9542c972ad87684abedd0
Dell <40-bis-0022@kdu.ac.lk>
2023-10-04 16:51:31 +0530

    First commit

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+class Student {
+    int id;
+    String name;
+
+    Student() {
+
+        System.out.println(id + " " + name);
+    }
+
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+
+        s1.display();
+        s2.display();
+    }
+}
